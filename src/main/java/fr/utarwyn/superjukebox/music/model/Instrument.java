7791d6e5f41e35bbc9388a8e933cdd4ec272eb90
utarwyn <maxime.malgorn@laposte.net>
2019-01-07 00:29:51 +0100

    Support for 1.13 Bukkit sounds

--- src/main/java/fr/utarwyn/superjukebox/music/model/Instrument.java
+++ src/main/java/fr/utarwyn/superjukebox/music/model/Instrument.java
@@ -1,6 +1,5 @@
 package fr.utarwyn.superjukebox.music.model;
 
-import fr.utarwyn.superjukebox.util.MinecraftVersion;
 import org.bukkit.Sound;
 
 /**
@@ -11,12 +10,12 @@ import org.bukkit.Sound;
  */
 public enum Instrument {
 
-	HARP(0, "NOTE_PIANO", "BLOCK_NOTE_HARP"),
-	BASS(1, "NOTE_BASS_GUITAR", "BLOCK_NOTE_BASS"),
-	BASEDRUM(2, "NOTE_BASS_DRUM", "BLOCK_NOTE_BASEDRUM"),
-	SNARE(3, "NOTE_SNARE_DRUM", "BLOCK_NOTE_SNARE"),
-	HAT(4, "NOTE_STICKS", "BLOCK_NOTE_HAT"),
-	GUITAR(5, "BLOCK_NOTE_GUITAR"),
+	HARP(0, "NOTE_PIANO", "BLOCK_NOTE_HARP", "BLOCK_NOTE_BLOCK_HARP"),
+	BASS(1, "NOTE_BASS", "BLOCK_NOTE_BASS", "BLOCK_NOTE_BLOCK_BASS"),
+	BASEDRUM(2, "NOTE_BASS_DRUM", "BLOCK_NOTE_BASEDRUM", "BLOCK_NOTE_BLOCK_BASEDRUM"),
+	SNARE(3, "NOTE_SNARE_DRUM", "BLOCK_NOTE_SNARE", "BLOCK_NOTE_BLOCK_SNARE"),
+	HAT(4, "NOTE_STICKS", "BLOCK_NOTE_HAT", "BLOCK_NOTE_BLOCK_HAT"),
+	GUITAR(5, "NOTE_BASS_GUITAR", "BLOCK_NOTE_GUITAR", "BLOCK_NOTE_BLOCK_GUITAR", "BLOCK_NOTE_BASS"),
 	FLUTE(6, "BLOCK_NOTE_FLUTE"),
 	BELL(7, "BLOCK_NOTE_BELL"),
 	CHIME(8, "BLOCK_NOTE_CHIME"),
@@ -24,41 +23,30 @@ public enum Instrument {
 
 	private int key;
 
-	private String pre1_9;
+	private String[] versionDependentNames;
 
-	private String post1_9;
+	private Sound bukkitSound;
 
-	private String post1_12;
-
-	private Sound mySound;
-
-	Instrument(int key, String pre1_9, String post1_9) {
+	Instrument(int key, String... versionDependentNames) {
 		this.key = key;
-		this.pre1_9 = pre1_9;
-		this.post1_9 = post1_9;
-	}
-
-	Instrument(int key, String post1_12) {
-		this.key = key;
-		this.post1_12 = post1_12;
+		this.versionDependentNames = versionDependentNames;
 	}
 
 	public Sound getSound() {
-		// Sound is cached to gain performance
-		if (this.mySound != null)
-			return this.mySound;
+		// If the sound is not stored in cache yet, we have to search for it
+		if (this.bukkitSound == null) {
+			for (String name : versionDependentNames) {
+				try {
+					return this.bukkitSound = Sound.valueOf(name);
+				} catch (IllegalArgumentException ignore) {
+					// Try the next one in the list
+				}
+			}
 
-		String sound;
-
-		if (MinecraftVersion.current() == MinecraftVersion.POST1_12) {
-			sound = (this.post1_12 != null) ? this.post1_12 : this.post1_9;
-		} else if (MinecraftVersion.current() == MinecraftVersion.PRE1_9) {
-			sound = this.pre1_9;
-		} else {
-			sound = this.post1_9;
+			throw new IllegalArgumentException("Sound of " + this.name() + " cannot be resolved.");
 		}
 
-		return this.mySound = Sound.valueOf(sound);
+		return this.bukkitSound;
 	}
 
 	public static Instrument get(byte key) {
