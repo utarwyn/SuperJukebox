1854dd476b512da64f6268f1962652f31a687bf0
utarwyn <maxime.malgorn@laposte.net>
2022-01-09 02:20:19 +0100

    Support Minecraft 1.17/1.18 (#23)

--- src/main/java/fr/utarwyn/superjukebox/util/ActionBarUtil.java
+++ src/main/java/fr/utarwyn/superjukebox/util/ActionBarUtil.java
@@ -1,7 +1,6 @@
 package fr.utarwyn.superjukebox.util;
 
 import fr.utarwyn.superjukebox.SuperJukebox;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -12,28 +11,51 @@ import java.util.logging.Level;
 
 /**
  * Used to send action message to players.
- * Thanks to ConnorLinfoot.
+ * Based on a class written by @ConnorLinfoot.
  *
  * @author Utarwyn
  * @since 0.1.0
  */
 public class ActionBarUtil {
 
-    private static final String NMS_PACKAGE = "net.minecraft.server.";
+    /**
+     * Package where NMS classes are stored. Used before 1.17
+     */
+    private static final String NMS_PACKAGE;
 
-    private static String serverVersion;
+    /**
+     * Package where Minecraft classes are stored. Used in 1.17+
+     */
+    private static final String MC_PACKAGE;
 
-    private static boolean useOldMethods;
+    /**
+     * Package where Craftbukkit classes are stored
+     */
+    private static final String CRAFTBUKKIT_PACKAGE;
+
+    /**
+     * Tell utility class to use pre 1.8 NMS objects
+     */
+    private static final boolean USE_PRE_18;
+
+    /**
+     * Network chat 1.17+ NMS package
+     */
+    private static final String NETWORK_CHAT = "network.chat";
 
     static {
-        serverVersion = Bukkit.getServer().getClass().getPackage().getName();
-        serverVersion = serverVersion.substring(serverVersion.lastIndexOf('.') + 1);
+        String version = ServerVersion.getBukkitVersion();
 
-        ActionBarUtil.useOldMethods = serverVersion.equalsIgnoreCase("v1_8_R1");
+        // Using remapped source code of Minecraft server? 1.17+
+        MC_PACKAGE = ServerVersion.isNewerThan(ServerVersion.V1_16) ? "net.minecraft." : null;
+        NMS_PACKAGE = "net.minecraft.server." + version + ".";
+        CRAFTBUKKIT_PACKAGE = "org.bukkit.craftbukkit." + version + ".";
+
+        USE_PRE_18 = version.equalsIgnoreCase("v1_8_R1");
     }
 
     private ActionBarUtil() {
-
+        // not implemented
     }
 
     public static void sendActionTitle(Player player, String message) {
@@ -85,21 +107,20 @@ public class ActionBarUtil {
 
     private static void sendActionTitlePre12(Player player, String message) {
         try {
-            Class<?> craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + serverVersion + ".entity.CraftPlayer");
+            Class<?> craftPlayerClass = getCraftbukkitClass("entity.CraftPlayer");
             Object craftPlayer = craftPlayerClass.cast(player);
             Object ppoc;
-            Class<?> c4 = Class.forName(NMS_PACKAGE + serverVersion + ".PacketPlayOutChat");
-            Class<?> c5 = Class.forName(NMS_PACKAGE + serverVersion + ".Packet");
+            Class<?> c3 = getNMSClass("IChatBaseComponent");
+            Class<?> c4 = getNMSClass("PacketPlayOutChat");
+            Class<?> c5 = getNMSClass("Packet");
 
-            if (useOldMethods) {
-                Class<?> c2 = Class.forName(NMS_PACKAGE + serverVersion + ".ChatSerializer");
-                Class<?> c3 = Class.forName(NMS_PACKAGE + serverVersion + ".IChatBaseComponent");
+            if (USE_PRE_18) {
+                Class<?> c2 = getNMSClass("ChatSerializer");
                 Method m3 = c2.getDeclaredMethod("a", String.class);
                 Object cbc = c3.cast(m3.invoke(c2, "{\"text\": \"" + message + "\"}"));
                 ppoc = c4.getConstructor(c3, byte.class).newInstance(cbc, (byte) 2);
             } else {
-                Class<?> c2 = Class.forName(NMS_PACKAGE + serverVersion + ".ChatComponentText");
-                Class<?> c3 = Class.forName(NMS_PACKAGE + serverVersion + ".IChatBaseComponent");
+                Class<?> c2 = getNMSClass("ChatComponentText");
                 Object o = c2.getConstructor(String.class).newInstance(message);
                 ppoc = c4.getConstructor(c3, byte.class).newInstance(o, (byte) 2);
             }
@@ -117,14 +138,14 @@ public class ActionBarUtil {
 
     private static void sendActionTitlePost12(Player player, String message) {
         try {
-            Class<?> craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + serverVersion + ".entity.CraftPlayer");
+            Class<?> craftPlayerClass = getCraftbukkitClass("entity.CraftPlayer");
             Object craftPlayer = craftPlayerClass.cast(player);
             Object ppoc;
-            Class<?> c4 = Class.forName(NMS_PACKAGE + serverVersion + ".PacketPlayOutChat");
-            Class<?> c5 = Class.forName(NMS_PACKAGE + serverVersion + ".Packet");
-            Class<?> c2 = Class.forName(NMS_PACKAGE + serverVersion + ".ChatComponentText");
-            Class<?> c3 = Class.forName(NMS_PACKAGE + serverVersion + ".IChatBaseComponent");
-            Class<?> chatMessageTypeClass = Class.forName(NMS_PACKAGE + serverVersion + ".ChatMessageType");
+            Class<?> c4 = getNMSClass("PacketPlayOutChat", "network.protocol.game");
+            Class<?> c5 = getNMSClass("Packet", "network.protocol");
+            Class<?> c2 = getNMSClass("ChatComponentText", NETWORK_CHAT);
+            Class<?> c3 = getNMSClass("IChatBaseComponent", NETWORK_CHAT);
+            Class<?> chatMessageTypeClass = getNMSClass("ChatMessageType", NETWORK_CHAT);
             Object[] chatMessageTypes = chatMessageTypeClass.getEnumConstants();
             Object chatMessageType = null;
             for (Object obj : chatMessageTypes) {
@@ -141,13 +162,39 @@ public class ActionBarUtil {
 
             Method m1 = craftPlayerClass.getDeclaredMethod("getHandle");
             Object h = m1.invoke(craftPlayer);
-            Field f1 = h.getClass().getDeclaredField("playerConnection");
+            Field f1;
+
+            // 1.17+ :: New way of retrieving player connection instance
+            if (ServerVersion.isNewerThan(ServerVersion.V1_16)) {
+                f1 = h.getClass().getField("b");
+            } else {
+                f1 = h.getClass().getField("playerConnection");
+            }
+
             Object pc = f1.get(h);
-            Method m5 = pc.getClass().getDeclaredMethod("sendPacket", c5);
+            Method m5 = getNMSDynamicMethod(pc.getClass(), "sendPacket", "a", c5);
             m5.invoke(pc, ppoc);
         } catch (Exception ex) {
             Log.log(Level.WARNING, "Cannot send action title packet (>=1.12) for " + player.getName() + "!", ex);
         }
+    }
+
+    private static Class<?> getCraftbukkitClass(String className) throws ClassNotFoundException {
+        return Class.forName(CRAFTBUKKIT_PACKAGE + className);
+    }
+
+    private static Class<?> getNMSClass(String className) throws ClassNotFoundException {
+        return Class.forName(NMS_PACKAGE + className);
+    }
+
+    private static Class<?> getNMSClass(String className, String package17)
+            throws ClassNotFoundException {
+        return Class.forName((MC_PACKAGE != null ? MC_PACKAGE + package17 + "." : NMS_PACKAGE) + className);
+    }
+
+    private static Method getNMSDynamicMethod(Class<?> clazz, String name, String name18, Class<?>... parameterTypes)
+            throws NoSuchMethodException {
+        return clazz.getMethod(ServerVersion.isNewerThan(ServerVersion.V1_17) ? name18 : name, parameterTypes);
     }
 
 }
--- src/main/java/fr/utarwyn/superjukebox/util/ServerVersion.java
+++ src/main/java/fr/utarwyn/superjukebox/util/ServerVersion.java
@@ -12,7 +12,9 @@ public enum ServerVersion {
     V1_13,
     V1_14,
     V1_15,
-    V1_16;
+    V1_16,
+    V1_17,
+    V1_18;
 
     private static ServerVersion currentVersion;
 
